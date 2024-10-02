81204d60f82369d2c53f00c9be3cc865a96035f9
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-10-02 08:56:07 -0400

    e

--- src/FuelCosts.java
+++ src/FuelCosts.java
@@ -6,20 +6,45 @@ public class FuelCosts {
         double priceForGallon = 0;
         double fuelEfficiency = 0;
         double gallonsOfFuel = 0;
-        double costPer100;
+        double cost4Per100;
         double distanceLeft;
-        boolean valid = false;
+        boolean fuelValid = false;
+        boolean mpgValid = false;
+        boolean priceValid = false;
 
         do {
             System.out.println("Enter Current Number of Gallons of Gasoline in Vehicle");
             if(scan.hasNextDouble()){
                 gallonsOfFuel = scan.nextDouble();
-                if(gallonsOfFuel >= 0 && gallonsOfFuel <= 63000){
-                    System.out.println("Enter Fuel Efficiency in MPG");
-                    if(scan.hasNextDouble()){
-                        fuelEfficiency = scan.nextDouble();
-                        if (fuelEfficiency > 0 && fuelEfficiency <= 14573){
-                            System.out.println("Enter current Price for Gallon of Gas");
+                if(gallonsOfFuel >= 0 && gallonsOfFuel <= 63000) {
+                    fuelValid = true;
+                    scan.nextLine();
+                } else {
+                    System.out.println("You have enter a number out of the range");
+                }
+            } else {
+                scan.nextLine();
+                System.out.println("You have entered the wrong data type");
+            }
+        } while (!fuelValid);
+
+        do {
+            System.out.println("Enter your mpg");
+            if(scan.hasNextDouble()) {
+                fuelEfficiency = scan.nextDouble();
+                if (fuelEfficiency > 0 && fuelEfficiency <= 14573) {
+                    mpgValid = true;
+                    scan.nextLine();
+                } else {
+                    System.out.println("You have enter a number out of the range");
+                }
+            } else {
+                scan.nextLine();
+                System.out.println("You have entered a wrong data type");
+            }
+        while (!mpgValid);
+
+                            System.out.println("Enter hi current Price for Gallon of Gas");
                             if(scan.hasNextDouble()){
                                 priceForGallon = scan.nextDouble();
                                 if(priceForGallon > 0){
@@ -29,7 +54,6 @@ public class FuelCosts {
                                 }
                             } else {
                                 scan.nextLine();
-                                scan.nextLine();
                                 System.out.println("You have entered in the wrong data type");
                             }
                         } else {
@@ -37,18 +61,12 @@ public class FuelCosts {
                         }
                     } else {
                         scan.nextLine();
-                        scan.nextLine();
                         System.out.println("You have entered in a wrong data type");
                     }
                 } else {
                     System.out.println("You have entered in a wrong range");
 
                 }
-
-            } else {
-                scan.nextLine();
-                System.out.println("You have entered the wrong data type");
-            }
         } while (!valid);
 
         distanceLeft = gallonsOfFuel * fuelEfficiency;
